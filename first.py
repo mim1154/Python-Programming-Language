d4306b7b7dcd2665da5031a7b74892768c4ea714
Mim <sabekunnaharmim6347@gmail.com>
2026-04-29 23:24:26 +0600

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+# print("python kopano lgbe" , "Mim you can do it")
+# print("Hey... I'm mim")
+# print(25+45)
+
+# name = "Mim"
+# age = 21
+# price = 3.03
+
+# print("My name is", name)
+# print(type(name))
+
+# a = 45
+# b = 56
+# print(a < b)
+
+# Type casting
+# a = int("2")
+# b = 4.25
+# print(a+b)
+
+# Input
+# name = input("Enter your name: ")
+# print("Welcome..",name)
+
+
+# Side = float(input("enter a num : "))
+# print("Area = ",Side*Side)
